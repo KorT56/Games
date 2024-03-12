02e37606b76123af2b09d4dbfa291fc95ea5bfb6
KorT56 <149665119+KorT56@users.noreply.github.com>
2024-03-12 09:37:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('dfdf');
+
+
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+console.log(nav);
+console.log(navBtn);
+console.log(navBtnImg);
+
+navBtn.onclick = () => {
+    if(nav.classList.toggle('open')) {
+       navBtnImg.src = "./Adaptive1920/Menu 2.png";
+    }else {
+        navBtnImg.src = "./Adaptive1920/Menu.png";
+    }
+}
+
